801a11867623b12e701b1597a4ee27f7ab5ad3ae
kurth4cker <kurth4cker@gmail.com>
2024-10-02 02:27:57 +0300

    main.go: add basic data structures

--- main.go
+++ main.go
@@ -4,30 +4,91 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os/exec"
 	"path/filepath"
 )
 
-// support only current dir for now
-var buildDir = "."
+type Compiler string
 
-var CC = "cc"
+func (c *Compiler) SetCommand(cmd string) {
+	*c = Compiler(cmd)
+}
+
+func (c *Compiler) Compile(src string) {
+}
+
+func (c *Compiler) Link(objects []string, output string) {
+}
+
+func (c *Compiler) CompileAndLink(sources []string, output string) {
+}
+
+// top struct which holds all project data
+type Project struct {
+	Name string
+
+	// Where binary will be put
+	BuildDir string
+
+	// C Compiler [cc]
+	CC Compiler
+
+	RootDir string // Project root
+	Executables []string
+	Libraries []string
+	IncludeDirs []string
+}
+
+func (p *Project) Build() {
+}
+
+func (p *Project) AddExecutable(exec string) {
+}
+
+func (p *Project) AddSource(src string) {
+}
+
+func (p *Project) AddLibrary(lib string) {
+}
+
+func (p *Project) AddIncludeDir(dir string) {
+}
+
+func (p *Project) SetBuildDir(dir string) {
+}
+
+func (p *Project) SetCC(cc *Compiler) {
+}
+
+func (p *Project) SetDefaults() {
+}
+
+func (p *Project) SetName(name string) {
+}
+
+func (p *Project) SetRootDir(dir string) {
+}
+
+var DefaultProject = Project {
+	BuildDir: ".",
+}
 
 func main() {
-	sourceFiles, _ := filepath.Glob("*.c")
+	var project Project
+	project.SetDefaults()
 
-	buildDir, err :=  filepath.Abs(buildDir)
+	// TODO: maybe these should be in project.SetDefaults
+	sourceFiles, _ := filepath.Glob("*.c")
+	for _, source := range sourceFiles {
+		project.AddSource(source)
+	}
+
+	// TODO: also these: project.SetDefaults?
+	buildDir, err :=  filepath.Abs(project.BuildDir)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	outputName := filepath.Base(buildDir)
+	project.SetBuildDir(buildDir)
 
-	var cmd = exec.Command(CC)
-	cmd.Args = append(cmd.Args, sourceFiles...)
-	cmd.Args = append(cmd.Args, "-o")
-	cmd.Args = append(cmd.Args, outputName)
-	cmd.Run()
-	fmt.Println(cmd)
+	project.Build()
 }
